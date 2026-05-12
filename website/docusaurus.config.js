d68a8eb70c3ca1ec421f776b819bc6b57a7c4c3c
Hitesh Jonnalagadda <jvsaihitesh@gmail.com>
2026-05-12 12:39:39 -0700

    Add Microsoft Clarity analytics placeholder

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -11,6 +11,19 @@ const config = {
   organizationName: 'hiteshsai',
   projectName: 'SMB-Suite-Training-Hub',
   onBrokenLinks: 'throw',
+  headTags: [
+    {
+      tagName: 'script',
+      attributes: { type: 'text/javascript' },
+      innerHTML: `
+        (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "YOUR_CLARITY_PROJECT_ID");
+      `,
+    },
+  ],
   markdown: {
     hooks: {
       onBrokenMarkdownLinks: 'warn',
